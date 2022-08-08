0315e107cabd1cf729f41f8a8e87876e83e6d3c4
duttasayan666 <84957859+duttasayan666@users.noreply.github.com>
2022-08-08 19:49:34 +0530

    This is a test class

--- src/test/java/com/techprimers/lazy/SpringBootLazyInitExampleApplicationTests.java
+++ src/test/java/com/techprimers/lazy/SpringBootLazyInitExampleApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootLazyInitExampleApplicationTests {
-
+//This is a test class
 	@Test
 	public void contextLoads() {
 	}
