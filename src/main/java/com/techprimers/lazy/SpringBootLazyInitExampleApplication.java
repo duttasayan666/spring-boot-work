887b66b2f1409959b230ee6a6533dc6f930e220e
sayan dutta <duttasayan666@gmail.com>
2022-08-08 20:33:07 +0530

    Spring start class

--- src/main/java/com/techprimers/lazy/SpringBootLazyInitExampleApplication.java
+++ src/main/java/com/techprimers/lazy/SpringBootLazyInitExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootLazyInitExampleApplication {
-
+//This Class is gonna start springbootapp
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLazyInitExampleApplication.class, args);
 	}
